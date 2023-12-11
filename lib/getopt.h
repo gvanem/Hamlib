3ea37750e2ef4ffaddc382079f5e0071344ed2cf
Gisle Vanem <gvanem@yahoo.no>
2023-12-11 14:47:56 +0100

    Remove silly Gnusim.

--- lib/getopt.h
+++ lib/getopt.h
@@ -77,11 +77,7 @@ extern int optopt;
 
 struct option
 {
-#if	__STDC__
   const char *name;
-#else
-  char *name;
-#endif
   /* has_arg can't be an enum because some compilers complain about
      type mismatches in all the code that assumes it is an int.  */
   int has_arg;
@@ -95,15 +91,10 @@ struct option
 #define required_argument	1
 #define optional_argument	2
 
-#if __STDC__
-#if defined(__GNU_LIBRARY__)
 /* Many other libraries have conflicting prototypes for getopt, with
    differences in the consts, in stdlib.h.  To avoid compilation
    errors, only prototype getopt for the GNU C library.  */
 extern int getopt (int argc, char *const *argv, const char *shortopts);
-#else /* not __GNU_LIBRARY__ */
-extern int getopt ();
-#endif /* not __GNU_LIBRARY__ */
 extern int getopt_long (int argc, char *const *argv, const char *options,
 		        const struct option *long_options, int *opt_index);
 extern int getopt_long_only (int argc, char *const *argv,
@@ -115,14 +106,6 @@ extern int _getopt_internal (int argc, char *const *argv,
 			     const char *optstring,
 		             const struct option *longopts, int *longind,
 			     int long_only);
-#else /* not __STDC__ */
-extern int getopt ();
-extern int getopt_long ();
-extern int getopt_long_only ();
-
-extern int _getopt_internal ();
-#endif /* not __STDC__ */
-
 #ifdef	__cplusplus
 }
 #endif
